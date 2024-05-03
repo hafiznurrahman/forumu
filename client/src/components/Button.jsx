91cd1f772f44e2b6dca16d828c019219eb0e4fe9
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-03 11:16:05 +0700

    create components input, button, and chatBubble. add components to HomePage and create server

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,3 +1,10 @@
 /*
 TODO: create button
 */
+
+export const SendButton = ({ children }) => {
+  return <button className="bg-gray-900 text-gray-50 rounded-lg p-2">{children}</button>;
+};
+export const InputFileButton = ({ children }) => {
+  return <button className="bg-gray-900 text-gray-50 rounded-lg p-2">{children}</button>;
+};
--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -2,3 +2,14 @@
 TODO: create chat bubble
       example: Chat GPT form code
 */
+
+export default function ChatBubble({ user, message, time }) {
+  return (
+    <section className="w-11/12 text-gray-50 bg-gray-900 divide-y divide-gray-500 rounded-xl">
+    
+      <strong className="block p-2" translate="no">{`@${user}`}</strong>
+      <p className="w-full block select-all font-mono p-4">{message}</p>
+      <small className="w-full block text-xs text-end italic p-2" translate="no">{time}</small>
+    </section>
+  );
+}
