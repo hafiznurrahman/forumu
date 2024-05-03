df215c14645a5cec3e0fc392baf9704b685bc64d
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-04 00:50:00 +0700

    message input functionality

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,9 +2,10 @@
 TODO: create button
 */
 
-export const SendButton = ({ children }) => {
-  return <button className="bg-gray-900 text-gray-50 rounded-lg p-2">{children}</button>;
-};
-export const InputFileButton = ({ children }) => {
-  return <button className="bg-gray-900 text-gray-50 rounded-lg p-2">{children}</button>;
-};
+export default function SendButton({ children, btnRef,click }) {
+  return (
+    <button className="bg-gray-900 text-gray-50 rounded-lg p-2 disabled:bg-gray-500" ref={btnRef} onClick={click}>
+      {children}
+    </button>
+  );
+}
