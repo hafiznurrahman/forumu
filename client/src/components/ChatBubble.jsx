2aa28c1ddfa2d10107592cc957f4360fe29f7e8d
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-04 17:54:27 +0700

    server

--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -8,7 +8,7 @@ export default function ChatBubble({ user, message, sendingTime }) {
     <section className="w-11/12 select-none text-gray-50 bg-gray-900 divide-y divide-gray-500 rounded-xl">
     
       <strong className="block  p-2" translate="no">{`@${user}`}</strong>
-      <p className="w-full block select-all p-4">{message}</p>
+      <p className="w-full block select-all break-all p-4">{message}</p>
       <small className="w-full block text-xs text-end italic p-2" translate="no">{sendingTime}</small>
     </section>
   );
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ TODO: add components chat bubble
       -------------- send button
 */
 
+import { io } from "socket.io-client";
 import { useState, useEffect, useRef } from "react";
 import ChatBubble from "../components/ChatBubble.jsx";
 import Button from "../components/Button.jsx";
@@ -17,12 +18,13 @@ export default function HomePage() {
   const messageInputRef = useRef(null);
   const sendBtnRef = useRef(null);
   const messageBoxAreaRef = useRef(null);
+  const socket = io("http://localhost:3000/");
 
-  useEffect(() => {
-    const sendBtnCurrent = sendBtnRef.current;
-    sendBtnCurrent.disabled = btnDisabled;
-  }, [btnDisabled]);
+  socket.on("connect", () => {
+    console.log("Connected to server");
+  });
 
+  /* 
   const handleInputChange = e => {
     const messageInputCurrent = messageInputRef.current;
     const messageInputValue = messageInputCurrent.value.trim();
@@ -36,70 +38,109 @@ export default function HomePage() {
     messageInputCurrent.style.height = messageInputCurrent.scrollHeight + "px";
   }, [msgVal]);
 
+  useEffect(() => {
+    const sendBtnCurrent = sendBtnRef.current;
+    sendBtnCurrent.disabled = btnDisabled;
+  }, [btnDisabled]);*/
+
   const handleSendBtn = () => {
     setBtnDisabled(true);
     const messageInputCurrent = messageInputRef.current;
     messageInputCurrent.blur();
-    setTimeout(() => {
-      bubbleElement("testing", msgVal, "00:00 - 00/00/0000");
-    }, 0);
+    socket.emit("message", messageInputCurrent.value);
+    bubbleElement("HUMAN", messageInputCurrent.value, "00:00 - 00/00/0000");
     messageInputCurrent.value = "";
     messageInputCurrent.style.height = "auto";
   };
 
-  const bubbleElement = (user, message, sendingTime) => {
+  const bubbleElement = (user, message) => {
     const messageBoxAreaCurrent = messageBoxAreaRef.current;
 
-    const section = document.createElement("section");
-    section.className =
-      "w-11/12 select-none text-gray-50 bg-gray-900 divide-y divide-gray-500 rounded-xl ml-auto";
+    const li = document.createElement("li");
+    li.className =
+      "w-[80%] h-auto border-l border-gray-900 border-dashed p-2 block relative ml-auto";
+
+    const div1 = document.createElement("div");
+    div1.className = "w-2 h-2 bg-gray-900 rounded-full absolute -top-1 -left-1";
 
     const strong = document.createElement("strong");
-    strong.className = "block p-2";
-    strong.textContent = `@${user}`;
-    strong.setAttribute("translate", "no");
+    strong.className =
+      "w-1/2 block text-xs text-center bg-gray-900 rounded-lg p-1 ml-auto mb-0.5 relative";
 
-    const paragraph = document.createElement("p");
-    paragraph.className = "w-full block select-all p-4";
-    paragraph.textContent = message;
+    const span = document.createElement("span");
+    span.className = "block truncate text-red-600";
+    span.textContent = user;
 
-    const small = document.createElement("small");
-    small.className = "w-full block text-xs text-end italic p-2";
-    small.textContent = sendingTime;
-    small.setAttribute("translate", "no");
+    const messageSpan = document.createElement("span");
+    messageSpan.className =
+      "block bg-gray-900 text-gray-50 rounded-lg hyphens-auto p-2";
+    messageSpan.textContent = message;
 
-    section.appendChild(strong);
-    section.appendChild(paragraph);
-    section.appendChild(small);
-    messageBoxAreaCurrent.appendChild(section);
+    strong.appendChild(span);
+
+    li.appendChild(div1);
+    li.appendChild(strong);
+    li.appendChild(messageSpan);
+    messageBoxAreaCurrent.appendChild(li);
 
     messageBoxAreaCurrent.scrollTop = messageBoxAreaCurrent.scrollHeight;
   };
+
+  socket.on("message", data => {
+    const messageBoxAreaCurrent = messageBoxAreaRef.current;
+
+    const li = document.createElement("li");
+    li.className =
+      "w-[80%] h-auto border-r border-gray-900 border-dashed p-2 block relative";
+
+    const div1 = document.createElement("div");
+    div1.className =
+      "w-2 h-2 bg-gray-900 rounded-full absolute -top-1 -right-1";
+
+    const strong = document.createElement("strong");
+    strong.className =
+      "w-1/2 block text-xs text-center bg-gray-900 rounded-lg p-1 mb-0.5 relative";
+
+    const span = document.createElement("span");
+    span.className = "block truncate text-blue-600";
+    span.textContent = "WHOAMI";
+
+    const messageSpan = document.createElement("span");
+    messageSpan.className =
+      "block bg-gray-900 text-gray-50 rounded-lg hyphens-auto p-2";
+    messageSpan.textContent = data;
+
+    strong.appendChild(span);
+
+    li.appendChild(div1);
+    li.appendChild(strong);
+    li.appendChild(messageSpan);
+    messageBoxAreaCurrent.appendChild(li);
+
+    messageBoxAreaCurrent.scrollTop = messageBoxAreaCurrent.scrollHeight;
+  });
 
   return (
     <>
       <section className="w-screen h-dvh p-3">
         <h1
-          className="w-screen text-3xl font-bold flex gap-3 fixed top-0 left-0 bg-gray-50 border-b border-gray-200 p-3"
+          className="w-screen text-3xl font-bold flex gap-3 fixed top-0 left-0 bg-gray-50 border-b border-gray-200 p-3 z-[999]"
           translate="no"
         >
           <MdForum size={35} /> FORUMU
         </h1>
 
-        <div
-          className="w-full h-full flex flex-col gap-4 overflow-y-auto scroll-smooth p-2 py-20 scrollHidden"
+        <ul
+          className="w-full h-full overflow-y-auto scroll-smooth p-2 py-20 scrollHidden"
           ref={messageBoxAreaRef}
-        >
-          
-
-        </div>
+        ></ul>
 
         <div className="w-full h-auto flex gap-3 items-end justify-between fixed bottom-0 left-0 bg-gray-50 border-t border-gray-200 p-3">
           <MessageInput
             id="messageInput"
             name="messageInput"
             placeholder="message"
-            inputChange={handleInputChange}
+            /* inputChange={handleInputChange}*/
             inputRef={messageInputRef}
           />
 
