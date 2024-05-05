ec4c8dd40b6547d9f8fbbd6660e0a408a5098852
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-05 20:33:02 +0700

    updated

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,148 +12,167 @@ import { MessageInput } from "../components/Input.jsx";
 import { MdForum, MdSend, MdImage, MdInfo } from "react-icons/md";
 
 export default function HomePage() {
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [msgVal, setMsgVal] = useState("");
-
   const messageInputRef = useRef(null);
   const sendBtnRef = useRef(null);
   const messageBoxAreaRef = useRef(null);
+
   const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3000/");
 
   socket.on("connect", () => {
     console.log("Connected to server");
   });
 
-  /* 
-  const handleInputChange = e => {
-    const messageInputCurrent = messageInputRef.current;
-    const messageInputValue = messageInputCurrent.value.trim();
-    setMsgVal(e.target.value);
-    setBtnDisabled(!messageInputValue);
-  };
-
-  useEffect(() => {
-    const messageInputCurrent = messageInputRef.current;
-    messageInputCurrent.style.height = "auto";
-    messageInputCurrent.style.height = messageInputCurrent.scrollHeight + "px";
-  }, [msgVal]);
-
-  useEffect(() => {
-    const sendBtnCurrent = sendBtnRef.current;
-    sendBtnCurrent.disabled = btnDisabled;
-  }, [btnDisabled]);*/
-
   const handleSendBtn = () => {
-    setBtnDisabled(true);
+    const now = new Date();
+    const currentHour = String(now.getHours()).padStart(2, "0");
+    const currentMinute = String(now.getMinutes()).padStart(2, "0");
+    const currentTime = `${currentHour}:${currentMinute}`;
+
     const messageInputCurrent = messageInputRef.current;
-    messageInputCurrent.blur();
-    socket.emit("message", messageInputCurrent.value);
-    bubbleElement("HUMAN", messageInputCurrent.value, "00:00 - 00/00/0000");
-    messageInputCurrent.value = "";
-    messageInputCurrent.style.height = "auto";
+    const checkInput = /\S/gm;
+
+    if (!checkInput.test(messageInputCurrent.value)) {
+      messageInputCurrent.focus();
+    } else {
+      messageInputCurrent.blur();
+      socket.emit("msgData", socket.id, messageInputCurrent.value, currentTime);
+      bubbleElement(socket.id, messageInputCurrent.value, currentTime);
+      messageInputCurrent.style.height = "auto";
+      messageInputCurrent.value = "";
+    }
   };
 
-  const bubbleElement = (user, message) => {
+  const bubbleElement = (username, message, time) => {
     const messageBoxAreaCurrent = messageBoxAreaRef.current;
 
-    const li = document.createElement("li");
-    li.className =
-      "w-[80%] h-auto border-l border-gray-900 border-dashed p-2 block relative ml-auto";
+    const listItem = document.createElement("li");
+    listItem.className = "w-full h-auto";
 
-    const div1 = document.createElement("div");
-    div1.className = "w-2 h-2 bg-gray-900 rounded-full absolute -top-1 -left-1";
+    const divWrapper = document.createElement("div");
+    divWrapper.className = "w-full grid grid-cols-8 gap-2";
+    listItem.appendChild(divWrapper);
 
-    const strong = document.createElement("strong");
-    strong.className =
-      "w-1/2 block text-xs text-center bg-gray-900 rounded-lg p-1 ml-auto mb-0.5 relative";
+    const imgAvatar = document.createElement("img");
+    imgAvatar.src = "/favicon.webp";
+    imgAvatar.alt = username;
+    imgAvatar.className =
+      "w-full h-auto aspect-square border rounded-full col-start-8";
+    divWrapper.appendChild(imgAvatar);
 
-    const span = document.createElement("span");
-    span.className = "block truncate text-red-600";
-    span.textContent = user;
+    const divContent = document.createElement("div");
+    divWrapper.appendChild(divContent);
+    divContent.className = "row-start-1 col-start-1 col-span-7";
 
-    const messageSpan = document.createElement("span");
-    messageSpan.className =
-      "block bg-gray-900 text-gray-50 rounded-lg hyphens-auto p-2";
-    messageSpan.textContent = message;
+    const divInner = document.createElement("div");
+    divInner.className = "w-full flex flex-row-reverse gap-2";
+    divContent.appendChild(divInner);
 
-    strong.appendChild(span);
+    const strongUsername = document.createElement("strong");
+    strongUsername.className =
+      "max-w-[50%] text-nowrap overflow-x-auto scrollHidden";
+    strongUsername.textContent = username;
+    divInner.appendChild(strongUsername);
 
-    li.appendChild(div1);
-    li.appendChild(strong);
-    li.appendChild(messageSpan);
-    messageBoxAreaCurrent.appendChild(li);
+    const spanTime = document.createElement("span");
+    spanTime.className = "text-gray-500";
+    spanTime.textContent = time;
+    divInner.appendChild(spanTime);
 
+    const divBackground = document.createElement("div");
+    divBackground.className = "bg-gray-900 rounded-lg p-3";
+    divContent.appendChild(divBackground);
+
+    const paragraph = document.createElement("p");
+    paragraph.className =
+      "select-all text-white whitespace-pre overflow-x-auto";
+    paragraph.textContent = message;
+    divBackground.appendChild(paragraph);
+
+    messageBoxAreaCurrent.appendChild(listItem);
     messageBoxAreaCurrent.scrollTop = messageBoxAreaCurrent.scrollHeight;
   };
 
-  socket.on("message", data => {
+  socket.on("msgData", (username, message, time) => {
     const messageBoxAreaCurrent = messageBoxAreaRef.current;
 
-    const li = document.createElement("li");
-    li.className =
-      "w-[80%] h-auto border-r border-gray-900 border-dashed p-2 block relative";
+    const listItem = document.createElement("li");
+    listItem.className = "w-full h-auto";
 
-    const div1 = document.createElement("div");
-    div1.className =
-      "w-2 h-2 bg-gray-900 rounded-full absolute -top-1 -right-1";
+    const divWrapper = document.createElement("div");
+    divWrapper.className = "w-full grid grid-cols-8 gap-2";
+    listItem.appendChild(divWrapper);
 
-    const strong = document.createElement("strong");
-    strong.className =
-      "w-1/2 block text-xs text-center bg-gray-900 rounded-lg p-1 mb-0.5 relative";
+    const imgAvatar = document.createElement("img");
+    imgAvatar.src = "/favicon.webp";
+    imgAvatar.alt = username;
+    imgAvatar.className =
+      "w-full h-auto aspect-square border rounded-full col-start-1";
+    divWrapper.appendChild(imgAvatar);
 
-    const span = document.createElement("span");
-    span.className = "block truncate text-blue-600";
-    span.textContent = "WHOAMI";
+    const divContent = document.createElement("div");
+    divWrapper.appendChild(divContent);
+    divContent.className = "row-start-1 col-start-2 col-span-7";
 
-    const messageSpan = document.createElement("span");
-    messageSpan.className =
-      "block bg-gray-900 text-gray-50 rounded-lg hyphens-auto p-2";
-    messageSpan.textContent = data;
+    const divInner = document.createElement("div");
+    divInner.className = "w-full flex gap-2";
+    divContent.appendChild(divInner);
 
-    strong.appendChild(span);
+    const strongUsername = document.createElement("strong");
+    strongUsername.className =
+      "max-w-[50%] text-nowrap overflow-x-auto scrollHidden";
+    strongUsername.textContent = username;
+    divInner.appendChild(strongUsername);
 
-    li.appendChild(div1);
-    li.appendChild(strong);
-    li.appendChild(messageSpan);
-    messageBoxAreaCurrent.appendChild(li);
+    const spanTime = document.createElement("span");
+    spanTime.className = "text-gray-500";
+    spanTime.textContent = time;
+    divInner.appendChild(spanTime);
 
+    const divBackground = document.createElement("div");
+    divBackground.className = "bg-gray-900 rounded-lg p-3";
+    divContent.appendChild(divBackground);
+
+    const paragraph = document.createElement("p");
+    paragraph.className =
+      "select-all text-white whitespace-pre overflow-x-auto";
+    paragraph.textContent = message;
+    divBackground.appendChild(paragraph);
+
+    messageBoxAreaCurrent.appendChild(listItem);
     messageBoxAreaCurrent.scrollTop = messageBoxAreaCurrent.scrollHeight;
   });
 
   return (
     <>
-      <section className="w-screen h-dvh p-3">
-        <h1
-          className="w-screen text-3xl font-bold flex gap-3 fixed top-0 left-0 bg-gray-50 border-b border-gray-200 p-3 z-[999]"
-          translate="no"
-        >
-          <MdForum size={35} />
-          FORUMU
-        </h1>
+      <h1
+        className="w-screen text-3xl font-bold flex gap-3 fixed top-0 left-0 bg-gray-50 border-b border-gray-200 p-3 z-[999]"
+        translate="no"
+      >
+        <MdForum size={35} />
+        FORUMU
+      </h1>
 
-        <ul
-          className="w-full h-full overflow-y-auto scroll-smooth p-2 py-20 scrollHidden"
-          ref={messageBoxAreaRef}
-        ></ul>
+      <ul
+        className="w-full h-full flex flex-col gap-4 overflow-y-auto scroll-smooth p-2 py-20 scrollHidden"
+        ref={messageBoxAreaRef}
+      ></ul>
 
-        <div className="w-full h-auto flex gap-3 items-end justify-between fixed bottom-0 left-0 bg-gray-50 border-t border-gray-200 p-3">
-          <MessageInput
-            id="messageInput"
-            name="messageInput"
-            placeholder="message"
-            /* inputChange={handleInputChange}*/
-            inputRef={messageInputRef}
-          />
+      <div className="w-full h-auto flex gap-3 items-end justify-between fixed bottom-0 left-0 bg-gray-50 border-t border-gray-200 p-3">
+        <MessageInput
+          id="messageInput"
+          name="messageInput"
+          placeholder="message"
+          inputRef={messageInputRef}
+        />
 
-          <Button>
-            <MdImage size={25} />
-          </Button>
+        <Button>
+          <MdImage size={25} />
+        </Button>
 
-          <Button btnRef={sendBtnRef} click={handleSendBtn}>
-            <MdSend size={25} />
-          </Button>
-        </div>
-      </section>
+        <Button btnRef={sendBtnRef} click={handleSendBtn}>
+          <MdSend size={25} />
+        </Button>
+      </div>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const { Server } = require("socket.io");
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain")
   res.end("Server is running");
 });
 
@@ -15,8 +14,9 @@ const io = new Server(server, {
 });
 io.on("connection", socket => {
   console.log("User connected");
-  socket.on("message", data => {
-    socket.broadcast.emit("message", data);
+  socket.on("msgData", (username, message, time) => {
+    socket.broadcast.emit("msgData", username, message,time);
+    
   });
   socket.on("disconnect", () => {
     console.log("User disconnected");
