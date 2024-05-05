e628f2aea4c8019aebcd1e097567c5479f5c8f6d
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-05 21:51:03 +0700

    update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT || "http://localhost:5173/",
     methods: ["GET", "POST"],
-    credentials: true
+    credentials: false
   }
 });
 
